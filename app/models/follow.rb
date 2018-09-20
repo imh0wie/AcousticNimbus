5007d07964ef860e16810e2ecb32c7baf0298a95
h0wiechan <howiechanhy@outlook.com>
2018-09-20 15:13:09 -0400

    Fixed validation in follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,6 @@
 class Follow < ApplicationRecord
   validates :followed_user_id, :follower_id, presence: true
-  validates [:followed_user_id, :follower_id], uniqueness: true
+  validates_uniqueness_of :followed_user_id, scope: :follower_id
 
   belongs_to :followed_user,
     foreign_key: :followed_user_id,
