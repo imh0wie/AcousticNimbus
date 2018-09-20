3bfd1847695717cb2e3c8b8b0667278476adb560
h0wiechan <howiechanhy@outlook.com>
2018-09-20 15:05:52 -0400

    Fixed syntax error in follow.rb  for heroku push

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,11 @@ class Follow < ApplicationRecord
   validates :followed_user_id, :follower_id, presence: true
   validates [:followed_user_id, :follower_id], uniqueness: true
 
-  belongs_to: :followed_user
-  belongs_to: :follower
+  belongs_to :followed_user,
+    foreign_key: :followed_user_id,
+    class_name: :User
+
+  belongs_to :follower,
+    foreign_key: :follower_id,
+    class_name: :User
 end
