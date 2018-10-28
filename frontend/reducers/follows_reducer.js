7641ab7ade45abaae8dcb98762ba5797783ae361
h0wiechan <howiechanhy@outlook.com>
2018-10-28 01:23:59 -0400

    Created reducer for follows

---
+++ frontend/reducers/follows_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_FOLLOWS } from "../actions/follow_actions";
+import { merge } from "lodash";
+
+const defaultState = {
+  id: null,
+  followedUserId: null,
+  followerId: null,
+};
+const followsReducer = (state = defaultState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_FOLLOWS:
+      return merge({}, action.follows);
+    default:
+      return state;
+  }
+};
+
+export default followsReducer;
