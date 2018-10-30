eec23a3f554780793aec6a6da823996a7efed2f3
h0wiechan <howiechanhy@outlook.com>
2018-10-29 20:42:10 -0400

    Created comment_api_util with ajax functions

---
+++ frontend/util/comment_api_util.js
@@ -0,0 +1,21 @@
+export const createComment = (comment) => {
+    return $.ajax({
+        method: "POST",
+        url: "/api/comments",
+        data: {comment},
+    });
+}
+
+export const deleteComment = (id) => {
+    return $.ajax({
+        method: "DELETE",
+        url: `/api/comments/${id}`,
+    })
+}
+
+export const fetchComments = () => {
+    return $.ajax({
+        method: "GET",
+        url: "/api/comments"
+    })
+}
