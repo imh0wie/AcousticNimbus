478af47e4f87739738f58d2a28b57b3870d0d468
h0wiechan <howiechanhy@outlook.com>
2018-10-29 20:28:23 -0400

    Created comments controller with database actions

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,33 @@
+class Api::CommentsController < ApplicationController
+    def index
+      @comments = Comment.all
+      render :index
+    end
+    
+    def create
+      @comment = Comment.new(comment_params)
+      if @comment.save
+        @comments = Follow.all
+        render :index
+      else
+        render @comment.errors.full_messages, status: 401
+      end
+    end
+  
+    def destroy
+      @comment = Comment.find(params[:id])
+      if @comment.destroy
+        @comments = Comment.all
+        render :index
+      else
+        render @comment.errors.full_messages, status: 401
+      end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:body, :song_id; :song_progress, :commenter_id)
+    end
+  end
+  
