76cf3484ec0f1bb682f3cd6297a940f564659dac
h0wiechan <howiechanhy@outlook.com>
2018-11-06 12:26:56 -0500

    Fixed styling for player bar

--- frontend/components/homepage/who_to_folow/who_to_follow.jsx
+++ frontend/components/homepage/who_to_folow/who_to_follow.jsx
@@ -4,10 +4,11 @@ import ArtistsList from "./artists_list";
 const WhoToFollow = () => {
     return (
         <div className="who-to-follow">
-            <div> className="header
-                <p><i className="fas fa-users"></i> Who to follow</p>
+            <div className="header"> 
+                <p><i className="fas fa-users"></i><i class="fas fa-user-friends"></i> Who to follow</p>
+                <p><i class="fas fa-redo-alt"></i> Refresh</p>
             </div>
-            <ArtistsList />
+            {/* <ArtistsList /> */}
         </div>
     );
 }
